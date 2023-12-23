04a95a6bd15b346f83a7cf60c72a60946ad31388
muhd-umer <thisismumer@gmail.com>
2023-12-23 18:18:53 +0500

    Vectorize the conversions

---
+++ utils/converter.py
@@ -0,0 +1,47 @@
+import struct
+
+import numpy as np
+
+
+class IEEE754:
+    """A class to represent the IEEE 754 floating point standard."""
+
+    def __init__(self, precision="single"):
+        """Initializes the IEEE 754 object with the specified precision.
+
+        Args:
+            precision (str): The desired precision, either "single" or "double".
+        """
+        if precision != "single":
+            raise NotImplementedError("Only single precision is currently supported.")
+        self.precision = precision
+
+    def float_to_binary(self, float_value):
+        """Converts a float to its unsigned binary representation using IEEE 754.
+
+        Args:
+            float_value (float): The float value to convert.
+
+        Returns:
+            str: The unsigned binary representation of the float value.
+        """
+        integer_part = np.vectorize(
+            lambda x: struct.unpack(">I", struct.pack(">f", x))[0]
+        )(float_value)
+        binary_string = np.vectorize(lambda x: f"{x:032b}")(integer_part)
+        return binary_string
+
+    def binary_to_float(self, binary_string):
+        """Converts an unsigned binary string to its float representation using IEEE 754.
+
+        Args:
+            binary_string (str): The unsigned binary string to convert.
+
+        Returns:
+            float: The float value represented by the binary string.
+        """
+        integer_part = np.vectorize(lambda x: int(x, 2))(binary_string)
+        float_value = np.vectorize(
+            lambda x: struct.unpack(">f", struct.pack(">I", x))[0]
+        )(integer_part)
+        return float_value
